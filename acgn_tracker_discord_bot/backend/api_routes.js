c99f58d8b00c6325fe76ab2aa35059bb7199db28
Sean Kao <seankao31@gmail.com>
2021-07-09 15:23:16 +0800

    api routes

---
+++ acgn_tracker_discord_bot/backend/api_routes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+let router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({
+        status: 'API is working',
+        message: 'Welcome to Acgn Tracker!'
+    });
+});
+
+export default router;
--- acgn_tracker_discord_bot/backend/index.js
+++ acgn_tracker_discord_bot/backend/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import api_routes from './api_routes.js';
+
+
 let app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send("Hello World Express"))
+app.use('/api', api_routes)
 
 app.listen(port, () => console.log("Running Acgn Tracker Backend on port " + port))
