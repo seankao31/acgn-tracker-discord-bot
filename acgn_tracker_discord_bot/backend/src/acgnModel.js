42f4bbbcb50bcc4b05892d1fa028dde63643662a
Sean Kao <seankao31@gmail.com>
2021-07-10 16:40:43 +0800

    schema index; add src directory

--- acgn_tracker_discord_bot/backend/src/acgnModel.js
+++ acgn_tracker_discord_bot/backend/src/acgnModel.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 let acgnSchema = mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   final_episode: {
     type: String,
--- acgn_tracker_discord_bot/backend/src/progressModel.js
+++ acgn_tracker_discord_bot/backend/src/progressModel.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 let progressSchema = mongoose.Schema({
   user: {
     type: String,
-    required: true
+    required: true,
+    index: true
   },
   acgn_id: {
     type: mongoose.Schema.Types.ObjectId,
@@ -17,6 +18,7 @@ let progressSchema = mongoose.Schema({
 }, {
   strict: 'throw'  // throws StrictModeError if additional field is provided
 });
+progressSchema.index({user: 1, acgn_id: 1}, {unique: true});
 
 let Progress = mongoose.model('progress', progressSchema);
 Progress.get = function (callback, limit) {
